21add1eec5c29826504efbc1c7a2c7cfdcc27dea
rtress0001 <rtress0001@gmail.com>
2023-07-29 20:54:18 -0500

    removed unnessary

--- Hello.py
+++ Hello.py
@@ -1,20 +1,3 @@
 var = "hello"
 newvar = "bye"
 print(var + newvar)
-
-# string.strip()
-name = "     John Smith     th"
-print(name.strip("ah"))
-
-#string.strip()
-name = "    John Smith    th"
-print(name.strip("ah"))
-
-# string.rstrip()
-name = "Bill Ross     th"
-print(name.rstrip(" " "th"))
-
-# String.rstrip()
-Name = "bill Ross   th"
-print(name.rstrip(" " "th"))
-                      
